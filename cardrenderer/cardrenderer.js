0bda4a487478066e946c6927f9a957a0f711f9f4
bobtheuberfish <87321962+bobtheuberfish@users.noreply.github.com>
2022-07-19 21:48:18 +0930

    Fix Reveal zoom sometimes not matching position with card

--- cardrenderer/cardrenderer.js
+++ cardrenderer/cardrenderer.js
@@ -902,7 +902,7 @@ var CardRenderer = {
 		if (this.forceIceRotation) destRot = Math.PI * 0.5;
         if (this.zoomed === true) {
 		  this.sprite.x = this.storedPosition.x;
-		  this.sprite.x = this.storedPosition.x;
+		  this.sprite.y = this.storedPosition.y;
           dispDiff.x = this.destinationPosition.x - this.storedPosition.x;
           dispDiff.y = this.destinationPosition.y - this.storedPosition.y;
         } else {
